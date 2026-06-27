131cf0ca11898cb4bf09b3888fed9e8d19f104d4
yatabashi <ijatabashi@gmail.com>
2026-06-27 19:51:55 +0900

    404更新

--- res/404/script.js
+++ res/404/script.js
@@ -1,4 +1,14 @@
-const CHRS = "abcdefghijklmnopqrstuvwxyz0123456789-";
-const randname = Array(Math.ceil(Math.random() * 18)).fill(undefined).map(() => CHRS[Math.floor(Math.random() * CHRS.length)]).join('');
-document.getElementById('url').href = `${randname}.html`;
-document.getElementById('url').textContent = `${randname}.html`;
+document.addEventListener("DOMContentLoaded", () => {
+    let quoteP = document.getElementById("quote");
+    let h = (new Date()).getHours();
+
+    if (0 <= h && h < 6) {
+        quoteP.innerText = "遠く離れたビーチに行かないか";
+    } else if (6 <= h && h < 12) {
+        quoteP.innerText = "阿耨多羅三藐三菩提のほとけたち\u200b我が立つ杣に冥加有らせ給へ";
+    } else if (12 <= h && h < 18) {
+        quoteP.innerText = "知らない角とか曲がったりして迷子になろう";
+    } else if (18 <= h && h < 24) {
+        quoteP.innerText = "Ku yàgg dox, yàgg gis.";
+    }
+});
