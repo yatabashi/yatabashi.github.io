d95f3933727261c8edffa528887a31da940710a6
yatabashi <ijatabashi@gmail.com>
2026-09-18 11:34:56 +0900

    背景画像にblur適用済画像を指定

--- res/index/bg.js
+++ res/index/bg.js
@@ -1,7 +1,15 @@
-let m = (new Date()).getMonth();
+let m = (new Date()).getMonth() + 1;
 
-if (2 <= m && m < 11) {  // 春〜秋: 3月〜11月
-    document.getElementById("bg-img").style.backgroundImage = "url('/res/index/yumihari-180.jpg')";
+if (3 <= m && m < 6) {
+    document.getElementById("bg-img").style.backgroundImage = "url('/res/index/yumihari-blur.jpg')";
+    document.getElementById("bg-overlay").style.backgroundColor = "rgba(255, 252, 249, 0.75)";
+} else if (6 <= m && m < 12) {
+    document.getElementById("bg-img").style.backgroundImage = "url('/res/index/yokaiti-blur.jpg')";
+    document.getElementById("bg-overlay").style.backgroundColor = "rgba(255, 252, 249, 0.8)";
+// } else if (9 <= m && m < 12) {
+//     document.getElementById("bg-img").style.backgroundImage = "url('/res/index/.jpg')";
+//     document.getElementById("bg-overlay").style.backgroundColor = "rgba(255, 252, 249, 0.8)";
 } else {  // 冬: 12月〜2月
-    document.getElementById("bg-img").style.backgroundImage = "url('/res/index/kamiazimi-180.jpg')";
+    document.getElementById("bg-img").style.backgroundImage = "url('/res/index/kamiazimi-blur.jpg')";
+    document.getElementById("bg-overlay").style.backgroundColor = "rgba(255, 252, 249, 0.7)";
 }
